946bee1d48c7e327ce7646db36f5a2de98cbf464
dimas-renanda <dimasrenanda@gmail.com>
2023-05-05 20:38:30 +0700

    cleanup

--- zcoba/register.php
+++ zcoba/register.php
@@ -1,77 +1,13 @@
 <?php
-/*
- *  Copyright (C) 2018 Laksamadi Guko.
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 require_once "conf/headhtml.php";
-require_once "conf/conn.php";
 session_start();
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
   header("location: ./dashboard");
   exit;
 }
 
-//session_start();
-
 ?>
 
-
-
-<!-- <div style="padding-top: 5%;"  class="login-box">
-  <div class="card"> -->
-    <!-- <div class="card-header">
-      <h3><?= $_please_login ?></h3>
-    </div> -->
-    <!-- <div class="card-body">
-      <div class="text-center pd-5">
-        <img src="assets/img/logo_s.png" height="100" alt="XNET Logo">
-      </div>
-      <div  class="text-center">
-      <b><span style="font-size: 25px; margin: 10px;">Moodle Tools</span></b>
-      </div>
-      <center>
-        <?php //require_once "signin.php"; ?>
-      <form autocomplete="off" action="signin.php" method="post">
-      <table class="table" style="width:90%">
-        <tr>
-          <td class="align-middle text-center">
-            <input style="width: 100%; height: 35px; font-size: 16px;" class="form-control" type="text" name="username" id="_username" placeholder="Username" required="1" autofocus>
-          </td>
-        </tr>
-        <tr>
-          <td class="align-middle text-center">
-            <input style="width: 100%; height: 35px; font-size: 16px;" class="form-control" type="password" name="password" placeholder="Password" required="1">
-          </td>
-        </tr>
-        <tr>
-          <td class="align-middle text-center">
-            <input style="width: 100%; margin-top:20px; height: 35px; font-weight: bold; font-size: 17px;" class="btn-login bg-primary pointer" type="submit" name="login" value="Login">
-          </td>
-        </tr>
-        <tr>
-          <td class="align-middle text-center">
-            <?= $error; ?>
-          </td>
-        </tr>
-      </table>
-      </form>
-      </center>
-    </div>
-  </div>
-</div> -->
 <style>
   body {
   background: #007bff;
@@ -95,7 +31,6 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
 }
 </style>
 
-<!-- =========================================================================== -->
   <div class="container">
     <div class="row">
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
@@ -134,25 +69,13 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
 <br>
               <div class="d-grid mb-2">
                 <a class="btn btn-secondary btn-login text-uppercase fw-bold" href="index.php">
-
-                  <!-- <i class="fab fa-google me-2"></i> -->
                   Sign In
-           
                 </a>
               </div>
 
               <hr class="my-4">
 
-              <!-- <div class="d-grid mb-2">
-                <button class="btn btn-google btn-login text-uppercase fw-bold" type="submit">
-                  <i class="fab fa-google me-2"></i> Sign in with Google
-                </button>
-              </div>
-              <div class="d-grid">
-                <button class="btn btn-facebook btn-login text-uppercase fw-bold" type="submit">
-                  <i class="fab fa-facebook-f me-2"></i> Sign in with Facebook
-                </button>
-              </div> -->
+
             </form>
 
 
