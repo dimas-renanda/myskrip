6e47ed902e993499e659f2f4c43207d14a029b31
dimas-renanda <dimasrenanda@gmail.com>
2023-05-11 18:01:24 +0700

    Update assets.php

--- zcoba/assets/assets.php
+++ zcoba/assets/assets.php
@@ -7,9 +7,9 @@
 
 
 <?php 
-echo '<link href="http://'.$_SERVER['HTTP_HOST'].'/xradius/crossradius-admin/assets/css/bootstrap.css" rel="stylesheet">';
-echo '<link href="http://'.$_SERVER['HTTP_HOST'].'/xradius/crossradius-admin/assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">';
-echo '<script src="http://'.$_SERVER['HTTP_HOST'].'/xradius/crossradius-admin/assets/js/bootstrap.bundle.min.js"></script>';
+echo '<link href="http://'.$_SERVER['HTTP_HOST'].'/myskrip/zcoba/assets/css/bootstrap.css" rel="stylesheet">';
+echo '<link href="http://'.$_SERVER['HTTP_HOST'].'/myskrip/zcoba/assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">';
+echo '<script src="http://'.$_SERVER['HTTP_HOST'].'/myskrip/zcoba/assets/js/bootstrap.bundle.min.js"></script>';
 ?>
 
 <link rel="icon" href="http://localhost/myskrip/zcoba/assets/img/logo_stab.png">
